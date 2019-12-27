4c40d7ecd62191ebe1301ff5e35da4a7b719eddb
simonpai <simon.pai@gmail.com>
2019-12-27 22:19:25 +0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const pathAPI = require('path');
+// const webpack = require('webpack');
+
+module.exports = (env, argv) => ({
+  mode: 'development',
+  context: pathAPI.resolve(__dirname),
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: pathAPI.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist'
+  },
+  devServer: {
+    open: true,
+    stats: 'minimal',
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    contentBase: pathAPI.resolve(__dirname, 'static')
+  }
+});
