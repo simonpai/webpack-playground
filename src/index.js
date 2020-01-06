602c2487ded21adf15a79d7d57b1ba99a4b8373e
simonpai <simon.pai@gmail.com>
2020-01-06 22:01:05 +0800

    Play with require.context()

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 require('./a');
 require('./b');
+
+var locales = require('./locale');
+
+console.log(locales.keys());
+console.log(locales('./en.json'));
+
+var animal = require('./animal');
+console.log(animal.keys());
+console.log(animal(animal.keys()[0]));
