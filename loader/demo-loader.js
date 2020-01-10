56dbfcf3831ae0a3cebe1f74f29d32858559f25d
Simon Pai <simon.pai@tenmax.io>
2020-01-10 09:40:08 +0800

    Introduce step mode

--- loader/demo-loader.js
+++ loader/demo-loader.js
@@ -1,9 +1,26 @@
+function displaySrc(line, lineNum) {
+  return 'displaysrc(' + JSON.stringify(line) + ', ' + lineNum + ');';
+}
+
+function pushToFns(block) {
+  return 'fns.push(function(){\n' + block + '\n})';
+}
+
+function shim(line) {
+  // cheat on local variable, as now it's wrapped into a function scope
+  return line.replace(/^var\s+/, 'window.');
+}
+
+const prefix = [
+  'var fns = window.fns || (window.fns = []);',
+  '!fns.i && (fns.i = 0);'
+].map(s => s + '\n').join('');
+
 module.exports = function(source) {
   const lines = [];
   let lineNum = 1;
   for (const line of source.split('\n')) {
-    lines.push('displaysrc(' + JSON.stringify(line) + ', ' + lineNum++ + ');\n');
-    lines.push(line);
+    lines.push(pushToFns(displaySrc(line, lineNum++) + '\n' + shim(line)));
   }
-  return lines.join('\n');
+  return prefix + lines.join('\n');
 };
--- src/polyfill/demo-runtime.js
+++ src/polyfill/demo-runtime.js
@@ -1,5 +1,15 @@
 var container = document.getElementById('display') || document.body;
 
+window.step = function() {
+  var fns = window.fns;
+  var fn = fns && fns[fns.i];
+  if (!fn) {
+    return;
+  }
+  fn();
+  fns.i++;
+};
+
 window.displaysrc = function(src, lineNum) {
   container.insertAdjacentHTML('beforeend',
     '<div class="source" data-line-num="' + lineNum + '"><pre><code>' + src + '&nbsp;</code></pre></div>');
